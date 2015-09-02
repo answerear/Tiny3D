01b6c2dfa42ea1b4b3f4a72c8dac4a8d0bf7a9f8
answerear <68071730@qq.com>
2015-09-02 17:25:15 +0800

    1. Finished cmake files to generate all project file

--- source/CMake/Utils/ProjectCMakeModule.cmake
+++ source/CMake/Utils/ProjectCMakeModule.cmake
@@ -28,10 +28,10 @@ endfunction(ADD_PROJECT_FILES)
 
 function(ADD_PROJECT_FILES_BY_EXT out_files group dir ext)
 	file(GLOB var ${dir}*${ext})
-	
+
 	foreach(f ${var})
 		get_filename_component(name ${f} NAME_WE)
-		ADD_PROJECT_FILES(file_list ${group} ${dir} "${name}${ext}")
+		add_project_files(file_list ${group} ${dir} "${name}${ext}")
 		list(APPEND temp_list ${file_list})
 	endforeach()
 
@@ -40,7 +40,7 @@ endfunction(ADD_PROJECT_FILES_BY_EXT)
 
 
 macro(SET_PROJECT_FILES group dir ext)
-	ADD_PROJECT_FILES_BY_EXT(out_files ${group} ${dir} ${ext})
+	add_project_files_by_ext(out_files ${group} ${dir} ${ext})
 	list(APPEND SOURCE_FILES ${out_files})
 endmacro(SET_PROJECT_FILES)
 	
