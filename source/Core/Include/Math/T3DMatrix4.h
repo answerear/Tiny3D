f7b96f5aa2b3397dd27c4feda6b19bd5fb7abd0b
answerear <68071730@qq.com>
2015-09-26 17:26:53 +0800

    1. Begin declaration of class Quaternion and Matrix4.

---
+++ source/Core/Include/Math/T3DMatrix4.h
@@ -0,0 +1,23 @@
+
+
+#ifndef __T3D_MATRIX4_H__
+#define __T3D_MATRIX4_H__
+
+
+#include "T3DEngineMacro.h"
+
+
+namespace Tiny3D
+{
+	class T3D_ENGINE_API Matrix4
+	{
+	public:
+		Matrix4();
+	};
+}
+
+
+#include "T3DMatrix4.inl"
+
+
+#endif	/*__T3D_MATRIX4_H__*/
---
+++ source/Core/Include/Math/T3DQuaternion.h
@@ -0,0 +1,20 @@
+
+
+#ifndef __T3D_QUATERNION_H__
+#define __T3D_QUATERNION_H__
+
+
+#include "T3DEngineMacro.h"
+
+
+namespace Tiny3D
+{
+	class T3D_ENGINE_API Quaternion
+	{
+	public:
+		Quaternion();
+	};
+}
+
+
+#endif	/*__T3D_QUATERNION_H__*/
