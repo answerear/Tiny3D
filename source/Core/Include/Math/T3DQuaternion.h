1afbe8aed098e49cea1a6149c136dd8684c372ca
answerear <68071730@qq.com>
2015-09-27 23:00:21 +0800

    1. Add some interfaces about declaration of class Quaternion.

--- source/Core/Include/Math/T3DQuaternion.h
+++ source/Core/Include/Math/T3DQuaternion.h
@@ -5,6 +5,8 @@
 
 
 #include "T3DEngineMacro.h"
+#include "T3DMath.h"
+#include "T3DMatrix3.h"
 
 
 namespace Tiny3D
@@ -12,7 +14,17 @@ namespace Tiny3D
 	class T3D_ENGINE_API Quaternion
 	{
 	public:
+		/// Default constructor, initialize to identity rotation.
 		Quaternion();
+		/// Create and initialize from an explicit list of values.
+		Quaternion(Real fW, Real fX, Real fY, Real fZ);
+		/// Create a quaternion from a rotation matrix.
+		Quaternion(const Matrix3 &matRot);
+		/// Create a quaternion from an angle and axis.
+		Quaternion(const Radian &rfAngle, const Vector3 &rkAxis);
+		/// Create a quaternion from 3 orthonormal local axis.
+		Quaternion(const Vector3 &rkXAxis, const Vector3 &rkYAxis, const Vector3 &rkZAxis);
+
 	};
 }
 
