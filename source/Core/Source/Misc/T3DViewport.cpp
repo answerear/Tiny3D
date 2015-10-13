f1932728f54dfd79015f47529963d537b19f8d12
answerear <68071730@qq.com>
2015-10-14 00:04:13 +0800

    1

---
+++ source/Core/Source/Misc/T3DViewport.cpp
@@ -0,0 +1,23 @@
+
+
+#include "T3DViewport.h"
+
+
+namespace Tiny3D
+{
+    Viewport::Viewport(SGCamera *pCamera, RenderTarget *pTarget, 
+        Real left, Real top, Real width, Real height, int32_t nZOrder)
+    {
+
+    }
+
+    Viewport::~Viewport()
+    {
+
+    }
+
+    void Viewport::update()
+    {
+
+    }
+}
