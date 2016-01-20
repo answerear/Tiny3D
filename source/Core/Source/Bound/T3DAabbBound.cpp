a5c3ac75e4079a1d7ffdefa4f711d6fda64dc2e3
answerear <68071730@qq.com>
2016-01-20 21:40:55 +0800

    Modified   source/Core/Source/Bound/T3DAabbBound.cpp

--- source/Core/Source/Bound/T3DAabbBound.cpp
+++ source/Core/Source/Bound/T3DAabbBound.cpp
@@ -68,13 +68,70 @@ namespace Tiny3D
 
     void AabbBound::updateBound(const Transform &transform)
     {
-        
-
+        // 这里不用传统的变换8个顶点，然后逐个比较获取最大x,y,z来重新设置AABB
+        // 这里使用快速变换的方法，减少矩阵变换带来的开销
+        const Vector3 &translate = transform.getTranslate();
         const Matrix4 &M = transform.getAffineMatrix();
-        Vector3 center = M * mAabb.getCenter();
-        
-        Vector3 TLF(mAabb.getMinX(), mAabb.getMaxY(), mAabb.getMaxZ());
 
+        Vector3 vMin = translate;
+        Vector3 vMax = translate;
+
+        // row 0
+        if (M[0][0] > Real(0.0))
+        {
+            vMin.x() += M[0][0] * mOriginalAabb.getMinX();
+            vMax.x() += M[0][0] * mOriginalAabb.getMaxX();
+        }
+        else
+        {
+            vMin.x() += M[0][0] * mOriginalAabb.getMaxX();
+            vMax.x() += M[0][0] * mOriginalAabb.getMinX();
+        }
+
+        if (M[0][1] > Real(0.0))
+        {
+            vMin.y() += M[0][1] * mOriginalAabb.getMinY();
+            vMax.y() += M[0][1] * mOriginalAabb.getMaxY();
+        }
+        else
+        {
+            vMin.y() += M[0][1] * mOriginalAabb.getMaxY();
+            vMax.y() += M[0][1] * mOriginalAabb.getMinY();
+        }
+
+        if (M[0][2] > Real(0.0))
+        {
+            vMin.z() += M[0][2] * mOriginalAabb.getMinZ();
+            vMax.z() += M[0][2] * mOriginalAabb.getMaxZ();
+        }
+        else
+        {
+            vMin.z() += M[0][2] * mOriginalAabb.getMaxZ();
+            vMax.z() += M[0][2] * mOriginalAabb.getMinZ();
+        }
+
+        // row 1
+        if (M[1][0] > Real(0.0))
+        {
+            vMin.x() += M[1][0] * mOriginalAabb.getMinX();
+            vMax.x() += M[1][0] * mOriginalAabb.getMaxX();
+        }
+        else
+        {
+            vMin.x() += M[1][0] * mOriginalAabb.getMaxX();
+            vMax.x() += M[1][0] * mOriginalAabb.getMinX();
+        }
+
+        if (M[1][1] > Real(0.0))
+        {
+            vMin.y() += M[1][1] * mOriginalAabb.getMinY();
+            vMax.y() += M[1][1] * mOriginalAabb.getMaxY();
+        }
+        else
+        {
+            vMin.y() += M[1][1] * mOriginalAabb.getMaxY();
+            vMax.y() += M[1][1] * mOriginalAabb.getMinY();
+        }
     }
 }
 
