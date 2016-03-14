6423b79e983c0a90f082969a7972f956559aea87
answerear <68071730@qq.com>
2016-03-14 20:24:19 +0800

    Modified   source/Tools/MeshConverter/mconv_command.cpp

--- source/Tools/MeshConverter/mconv_command.cpp
+++ source/Tools/MeshConverter/mconv_command.cpp
@@ -37,20 +37,15 @@ namespace mconv
         printf("\n");
         printf("Options:\n");
         printf("-?       : Display this help information.\n");
-#ifdef ALLOW_INPUT_TYPE
         printf("-i <type>: Set the type of the input file to <type>\n");
-#endif
+        printf("\t<type> : This type should be FBX (fbx), T3DB (t3db), T3DT (t3dt) or DAE (dae).\n");
         printf("-o <type>: Set the type of the output file to <type>\n");
-        printf("-f       : Flip the V texture coordinates.\n");
-        printf("-p       : Pack vertex colors to one float.\n");
-        printf("-m <size>: The maximum amount of vertices or indices a mesh may contain (default: 32k)\n");
-        printf("-b <size>: The maximum amount of bones a nodepart can contain (default: 12)\n");
-        printf("-w <size>: The maximum amount of bone weights per vertex (default: 4)\n");
+        printf("\t<type> : This type should be FBX (fbx), T3DB (t3db), T3DT (t3dt), T3D (t3d) or DAE (dae).\n");
         printf("-v       : Verbose: print additional progress information\n");
         printf("\n");
         printf("<input>  : The filename of the file to convert.\n");
         printf("<output> : The filename of the converted file.\n");
         printf("\n");
-        printf("<type>   : FBX, G3DJ (json) or G3DB (binary).\n");
+        printf("<type>   : FBX, T3DB (binary) or T3DT (xml).\n");
     }
 }
