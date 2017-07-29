121e4ae907888aa732dd807d730ce2999a9bebb7
answerear <68071730@qq.com>
2017-07-29 19:17:46 +0800

    1、【All】给所有源码加入software license说明

--- source/Samples/transformation/TransformationApp.cpp
+++ source/Samples/transformation/TransformationApp.cpp
@@ -1,4 +1,11 @@
-
+/*******************************************************************************
+ * This file is part of Tiny3D (Tiny 3D Graphic Rendering Engine)
+ * Copyright (C) 2015-2017  Aaron Wong
+ * For latest info, see https://github.com/asnwerear/Tiny3D
+ *
+ * You may use this sample code for anything you like, it is not covered by the
+ * same license as the rest of the engine.
+*******************************************************************************/
 
 #include "TransformationApp.h"
 
