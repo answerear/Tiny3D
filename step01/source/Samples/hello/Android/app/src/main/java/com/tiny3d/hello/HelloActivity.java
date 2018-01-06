cee350550cf658e4b67ef821a01ea874aa642b4b
answerear <68071730@qq.com>
2018-01-06 16:14:56 +0800

    【step01】 1、重新提交之前没有提交的文件

--- step01/source/Samples/hello/Android/app/src/main/java/com/tiny3d/hello/HelloActivity.java
+++ step01/source/Samples/hello/Android/app/src/main/java/com/tiny3d/hello/HelloActivity.java
@@ -3,7 +3,7 @@ package com.tiny3d.hello;
 import com.tiny3d.lib.Tiny3DActivity;
 
 /**
- * Created by aaronwang on 2018/1/5.
+ * Created by aaronwang on 2018/1/6.
  */
 
 public class HelloActivity extends Tiny3DActivity {
