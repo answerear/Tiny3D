7179fdbb2f179844100e2e70bb57d5fd84d7532b
answerear <68071730@qq.com>
2018-01-09 11:32:19 +0800

    【step01】 1、添加对ios的支持

---
+++ step01/source/build_ios.sh
@@ -0,0 +1,5 @@
+rm -rf build_ios
+mkdir -p build_ios && cd build_ios
+cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=../CMake/Toolchains/iOS.cmake -DIOS_PLATFORM=SIMULATOR -DCMAKE_OSX_ARCHITECTURES=x86_64 ../
+cd ..
+# cmake --build build_ios --config Release
