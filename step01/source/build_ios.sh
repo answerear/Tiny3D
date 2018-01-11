c51a7b2e3908401b37158e3f2703e7b2c8fdc4b3
answerear <68071730@qq.com>
2018-01-11 21:33:29 +0800

    【step01】 1、iOS真机构建

--- step01/source/build_ios.sh
+++
@@ -1,6 +0,0 @@
-rm -rf build_ios
-mkdir -p build_ios && cd build_ios
-cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=../CMake/Toolchains/iOS.cmake -DIOS_PLATFORM=SIMULATOR -DCMAKE_OSX_ARCHITECTURES=x86_64 ../
-open Tiny3D.xcodeproj
-cd ..
-# cmake --build build_ios --config Release
