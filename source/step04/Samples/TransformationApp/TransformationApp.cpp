ef4dd360590a86b5dc5161e727db8e4114f42e5a
answerear <68071730@qq.com>
2018-07-02 00:10:23 +0800

    Modified   doc/images/painter.xlsx Modified   doc/一起动手实现3D渲染引擎.md Modified   source/step04/Samples/TransformationApp/TransformationApp.cpp Add        doc/images/matrix-3D-aperture-imaging.fbx Add        doc/images/matrix-3D-orth-projection.fbx Add        doc/images/matrix-3D-perp-projection- 01.fbx Add        doc/images/matrix-3D-perp-projection.fbx Add        doc/images/matrix-projection-plane.fbx Add        doc/images/project-plane.png Add        doc/images/white.png

--- source/step04/Samples/TransformationApp/TransformationApp.cpp
+++ source/step04/Samples/TransformationApp/TransformationApp.cpp
@@ -75,18 +75,58 @@ bool TransformationApp::applicationDidFinishLaunching()
     m3[1][0] = -0.707, m3[1][1] = 1.25, m3[1][2] = 0;
     m3[2][0] = 0, m3[2][1] = 0, m3[2][2] = 1;
 
-    Matrix3 Q;
+    Matrix3 R;
     Vector3 S, T;
-    m3.QDUDecomposition(Q, S, T);
+    m3.QDUDecomposition(R, S, T);
     
     Vector3 axis;
     Radian radians;
-    Q.toAxisAngle(axis, radians);
+    R.toAxisAngle(axis, radians);
     degree = radians;
 
     Matrix3 Mr(Vector3::UNIT_Z, Radian(Degree(45)));
     Matrix3 Ms(S[0], S[1], S[2]);
-    Matrix3 R = Mr * Ms;
+    R = Mr * Ms;
+
+    m4[0][0] = Math::sqrt(2); m4[0][1] = Math::sqrt(2); m4[0][2] = REAL_ZERO; m4[0][3] = -2;
+    m4[1][0] = -Math::sqrt(2); m4[1][1] = Math::sqrt(2); m4[1][2] = REAL_ZERO; m4[1][3] = -2;
+    m4[2][0] = REAL_ZERO; m4[2][1] = REAL_ZERO; m4[2][2] = 2; m4[2][3] = REAL_ZERO;
+    m4[3][0] = REAL_ZERO; m4[3][1] = REAL_ZERO; m4[3][2] = REAL_ZERO; m4[3][3] = REAL_ONE;
+
+    Quaternion Q;
+    m4.decomposition(T, S, Q);
+    Q.toRotationMatrix(R);
+
+    Vector3 T1, S1;
+    Quaternion Q1;
+    Matrix3 R1;
+    T1[0] = m4[0][3]; T1[1] = m4[1][3]; T1[2] = m4[2][3];
+    Real length = Math::sqrt(m4[0][0] * m4[0][0] + m4[1][0] * m4[1][0] + m4[2][0] * m4[2][0]);
+    Real invLength = REAL_ONE / length;
+    R1[0][0] = m4[0][0] * invLength; 
+    R1[1][0] = m4[1][0] * invLength; 
+    R1[2][0] = m4[2][0] * invLength;
+
+    S1[0] = length;
+
+    length = Math::sqrt(m4[0][1] * m4[0][1] + m4[1][1] * m4[1][1] + m4[2][1] * m4[2][1]);
+    invLength = REAL_ONE / length;
+    R1[0][1] = m4[0][1] * invLength;
+    R1[1][1] = m4[1][1] * invLength;
+    R1[2][1] = m4[2][1] * invLength;
+
+    S1[1] = length;
+
+    invLength = Math::sqrt(m4[0][2] * m4[0][2] + m4[1][2] * m4[1][2] + m4[2][2] * m4[2][2]);
+    invLength = REAL_ONE / length;
+    R1[0][2] = m4[0][2] * invLength;
+    R1[1][2] = m4[1][2] * invLength;
+    R1[2][2] = m4[2][2] * invLength;
+
+    S1[2] = length;
+
+    Q1.fromRotationMatrix(R1);
+
     return true;
 }
 
