588c422b3d65cc0bba002b01ccb52cc66f2bb114
answerear <68071730@qq.com>
2018-12-24 23:38:45 +0800

    Modified   source/step06/Platform/Include/Memory/T3DConstruct.h

--- source/step06/Platform/Include/Memory/T3DConstruct.h
+++ source/step06/Platform/Include/Memory/T3DConstruct.h
@@ -17,25 +17,59 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
-#ifndef __T3D_ALLOCATOR_H__
-#define __T3D_ALLOCATOR_H__
+#ifndef __T3D_CONSTRUCT_H__
+#define __T3D_CONSTRUCT_H__
 
 
 #include "T3DPlatformPrerequisites.h"
-
+#include <new.h>
 
 namespace Tiny3D
 {
-    template <typename T, typename Alloc>
-    class Allocator
+    template <typename T1, typename T2>
+    inline void _Construct(T1 *p, const T2 &value)
     {
-    public:
-        static T *allocate(size_t n);
-        static T *allocate();
-        static void deallocate(T *p, size_t n);
-        static void deallocate(T *p)
-    };
+        new ((void *)p) T1(value);
+    }
+
+    template <typename T>
+    inline void _Construct(T *p)
+    {
+        new ((void *)p) T();
+    }
+
+    template <typename T>
+    inline void _Destroy(T *pointer)
+    {
+        pointer->~T();
+    }
+
+    template <typename ForwardIterator>
+    inline void __Destroy_aux(ForwardIterator first, ForwardIterator last, __false_type)
+    {
+        for (; first != last; ++first)
+            _Destroy(&*first);
+    }
+
+    template <typename ForwardIterator>
+    inline void __Destroy_aux(ForwardIterator, ForwardIterator last, __true_type)
+    {
+
+    }
+
+    template <typename ForwardIterator, typename T>
+    inline void __Destroy(ForwardIterator first, ForwardIterator last, T *)
+    {
+        typedef typename __type_traits<T>::has_trivial_destructor _Trivial_destructor;
+        __Destroy_aux(first, last, _Trivial_destructor(first));
+    }
+
+    template <typename ForwardIterator>
+    inline void __Destroy(ForwardIterator first, ForwardIterator last)
+    {
+
+    }
 }
 
 
-#endif  /*__T3D_ALLOCATOR_H__*/
+#endif  /*__T3D_CONSTRUCT_H__*/
