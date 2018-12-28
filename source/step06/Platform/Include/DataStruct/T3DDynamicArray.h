8e3bb215ea588ca1149ac34ab3d5e355c04be458
answerear <68071730@qq.com>
2018-12-28 17:22:01 +0800

    Missing    source/step06/Platform/Include/DataStruct/T3DVector.h Modified   source/step06/Platform/Include/Memory/T3DAllocator.h Add        source/step06/Platform/Include/DataStruct/T3DDynamicArray.h

--- source/step06/Platform/Include/DataStruct/T3DDynamicArray.h
+++ source/step06/Platform/Include/DataStruct/T3DDynamicArray.h
@@ -17,8 +17,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
-#ifndef __T3D_VECTOR_H__
-#define __T3D_VECTOR_H__
+#ifndef __T3D_DYNAMIC_ARRAY_H__
+#define __T3D_DYNAMIC_ARRAY_H__
 
 
-#endif  /*__T3D_VECTOR_H__*/
+#endif  /*__T3D_DYNAMIC_ARRAY_H__*/
--- source/step06/Platform/Include/Memory/T3DAllocator.h
+++ source/step06/Platform/Include/Memory/T3DAllocator.h
@@ -22,18 +22,100 @@
 
 
 #include "T3DPlatformPrerequisites.h"
+#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
 
 
 namespace Tiny3D
 {
+    extern void (*__malloc_alloc_oom_handler)();
+
     template <typename T, typename Alloc>
-    class Allocator
+    class allocator
     {
     public:
         static T *allocate(size_t n);
         static T *allocate();
         static void deallocate(T *p, size_t n);
         static void deallocate(T *p)
+    };
+
+    class __malloc_alloc
+    {
+    public:
+        static void *allocate(size_t n)
+        {
+            void *result = malloc(n);
+            if (0 == result)
+                _oom_malloc(n);
+            return result;
+        }
+
+        static void deallocate(void *p, size_t/* n*/)
+        {
+            free(p);
+        }
+
+        static void *reallocate(void *p, size_t/* old_size*/, size_t new_size)
+        {
+            void *result = realloc(p, new_size);
+            if (0 == result)
+                _oom_realloc(p, new_size);
+            return result;
+        }
+
+        static void(*_set_malloc_handler(void (*_f)()))()
+        {
+            void (*__old)() = __malloc_alloc_oom_handler;
+            __malloc_alloc_oom_handler = _f;
+            return (__old);
+        }
+
+    private:
+        static void *_oom_malloc(size_t n)
+        {
+            void (*my_malloc_handler)();
+            void *result;
+            while (1)
+            {
+                my_malloc_handler = __malloc_alloc_oom_handler;
+                if (0 == my_malloc_handler)
+                {
+                    fprintf(stderr, "out of memory !\n");
+                    exit(1);
+                }
+
+                (*my_malloc_handler)();
+                result = malloc(n);
+                if (result)
+                    break;
+            }
+
+            return result;
+        }
+
+        static void *_oom_realloc(void *p, size_t n)
+        {
+            void(*my_malloc_handler)();
+            void *result;
+            while (1)
+            {
+                my_malloc_handler = __malloc_alloc_oom_handler;
+                if (0 == my_malloc_handler)
+                {
+                    fprintf(stderr, "out of memory !\n");
+                    exit(1);
+                }
+
+                (*my_malloc_handler)();
+                result = realloc(p, n);
+                if (result)
+                    break;
+            }
+
+            return result;
+        }
     };
 }
 
