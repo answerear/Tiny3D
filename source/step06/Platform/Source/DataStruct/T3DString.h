306e2c3fc646569579c0906a85e0958f90894a06
answerear <68071730@qq.com>
2019-01-03 00:11:45 +0800

    Modified   source/step06/Platform/CMakeLists.txt Add        source/step06/Platform/Include/DataStruct/T3DDynamicArray.h Add        source/step06/Platform/Include/DataStruct/T3DHash.h Add        source/step06/Platform/Include/DataStruct/T3DIteratorTraits.h Add        source/step06/Platform/Include/DataStruct/T3DList.h Add        source/step06/Platform/Include/DataStruct/T3DMap.h Add        source/step06/Platform/Include/DataStruct/T3DQueue.h Add        source/step06/Platform/Include/DataStruct/T3DRBTree.h Add        source/step06/Platform/Include/DataStruct/T3DSet.h Add        source/step06/Platform/Include/DataStruct/T3DStack.h Add        source/step06/Platform/Include/DataStruct/T3DString.h Add        source/step06/Platform/Include/DataStruct/T3DTypeTraits.h Add        source/step06/Platform/Include/Memory/T3DConstruct.h Add        source/step06/Platform/Include/Memory/T3DDefaultAllocator.h Add        source/step06/Platform/Include/Memory/T3DMallocAllocator.h Add        source/step06/Platform/Include/Memory/T3DUninitialized.h Add        source/step06/Platform/Source/DataStruct/T3DString.h Add        source/step06/Platform/Source/Memory/T3DDefaultAllocator.cpp Add        source/step06/Platform/Source/Memory/T3DMallocAllocator.cpp

---
+++ source/step06/Platform/Source/DataStruct/T3DString.h
@@ -0,0 +1,78 @@
+﻿/*******************************************************************************
+ * This file is part of Tiny3D (Tiny 3D Graphic Rendering Engine)
+ * Copyright (C) 2015-2017  Answer Wong
+ * For latest info, see https://github.com/asnwerear/Tiny3D
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+#ifndef __T3D_TYPE_H__
+#define __T3D_TYPE_H__
+
+#include <typeinfo>
+#include <string>
+#include <memory>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <string.h>
+#include <memory.h>
+
+
+typedef signed char         char_t;
+typedef unsigned char       uchar_t;
+typedef signed short        short_t;
+typedef unsigned short      ushort_t;
+typedef signed int          int_t;
+typedef unsigned int        uint_t;
+typedef signed long         long_t;
+typedef unsigned long       ulong_t;
+
+typedef signed char         int8_t;
+typedef unsigned char       uint8_t;
+typedef signed short        int16_t;
+typedef unsigned short      uint16_t;
+typedef signed int          int32_t;
+typedef unsigned int        uint32_t;
+
+#if !defined (T3D_OS_ANDROID) && !defined (T3D_OS_LINUX)
+typedef signed long long    int64_t;
+typedef unsigned long long  uint64_t;
+#endif
+
+
+typedef std::string         String;
+typedef std::wstring        WString;
+
+typedef std::string         UTF8String;
+typedef std::u16string      UTF16String;
+typedef std::u32string      UTF32String;
+
+typedef void*               THandle;
+
+typedef std::thread                     TThread;
+typedef std::mutex                      TMutex;
+typedef std::recursive_mutex            TRecursiveMutex;
+typedef std::condition_variable         TCondVariable;
+
+template <typename T>
+using TAutoLock = std::unique_lock<T>;
+
+
+#endif  /*__T3D_TYPE_H__*/
