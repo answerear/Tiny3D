f9276f81e09bd2c793136f05d58a62b4a417eeb7
answerear <68071730@qq.com>
2019-01-08 22:07:28 +0800

    1、修改版权信息

--- source/step07/Core/Source/Kernel/T3DEngine.cpp
+++ source/step07/Core/Source/Kernel/T3DEngine.cpp
@@ -1,6 +1,6 @@
 ﻿/*******************************************************************************
  * This file is part of Tiny3D (Tiny 3D Graphic Rendering Engine)
- * Copyright (C) 2015-2017  Answer Wong
+ * Copyright (C) 2015-2019  Answer Wong
  * For latest info, see https://github.com/asnwerear/Tiny3D
  *
  * This program is free software: you can redistribute it and/or modify
@@ -89,7 +89,12 @@ namespace Tiny3D
                 break;
             }
 
+            // 加载配置文件
             ret = loadConfig(config);
+            if (ret != T3D_ERR_OK)
+            {
+                break;
+            }
 
             // 创建渲染窗口
             ret = createRenderWindow("Demo");
