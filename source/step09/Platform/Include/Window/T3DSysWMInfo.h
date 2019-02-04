f1b7eee85c57bf979a8daea6099411dec456e5b6
answerear <68071730@qq.com>
2019-02-05 00:36:09 +0800

    【step09】 1、调通 Linux 平台加载图标

--- source/step09/Platform/Include/Window/T3DSysWMInfo.h
+++ source/step09/Platform/Include/Window/T3DSysWMInfo.h
@@ -1,4 +1,4 @@
-﻿/*******************************************************************************
+/*******************************************************************************
  * This file is part of Tiny3D (Tiny 3D Graphic Rendering Agent)
  * Copyright (C) 2015-2019  Answer Wong
  * For latest info, see https://github.com/asnwerear/Tiny3D
@@ -38,7 +38,7 @@ namespace Tiny3D
 #elif defined (T3D_OS_LINUX)
         // Linux X11
         void        *display;       /**< The X11 display (Display) */
-        void        *window;        /**< The X11 window (Window) */
+        ulong_t     window;        /**< The X11 window (Window) */
 #elif defined (T3D_OS_OSX)
         // Mac OS X cocoa
         void        *window;        /**< The Cocoa window (NSWindow) */
