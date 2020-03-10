41b0943bcb532f4e94557bca8f40249696243dfe
answerear <68071730@qq.com>
2020-03-10 09:44:32 +0800

    【step09】加入对 proto buffer 的支持

--- tools/protobuf/Windows/bin/generate_source.bat
+++ tools/protobuf/Windows/bin/generate_source.bat
@@ -1,6 +1,13 @@
+@rem generate proto buffer source files.
 @set input_file=MaterialScriptObject.proto
-@set output_dir=..\..\..\..\source\step09\Tools\ScriptCompiler\protobuf\
-del /Q %output_dir%
 .\protoc.exe --proto_path="../../proto" --cpp_out="../../source" %input_file%
-xcopy /Y ..\..\source\*.pb.* %output_dir%
-pause
+
+@rem copy all generated files to target directory.
+@set tools_dir=..\..\..\..\source\step09\Tools\ScriptCompiler\protobuf\
+@set core_dir=..\..\..\..\source\step09\Core\protobuf\
+
+@del /Q %tools_dir%
+@xcopy /Y ..\..\source\*.pb.* %tools_dir%
+@del /Q %core_dir%
+@xcopy /Y ..\..\source\*.pb.* %core_dir%
+@pause
