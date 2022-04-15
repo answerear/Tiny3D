865c038c20ab1aab2757b703e93765c5d5f435ac
answerear <68071730@qq.com>
2022-04-15 19:32:11 +0800

    modifed mac os x building script.

--- source/Projects/generate-xcode-osx.sh
+++ source/Projects/generate-xcode-osx.sh
@@ -1,9 +1,9 @@
 cd ../
 rm -rf xcode-osx
-rm -rf bin
-rm -rf lib
 mkdir -p xcode-osx && cd xcode-osx
-cmake -G "Xcode" ../
+CMAKE_C_COMPILER=$(xcrun -find cc)
+CMAKE_CXX_COMPILER=$(xcrun -find c++)
+cmake -D CMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER -D CMAKE_C_COMPILER=$CMAKE_C_COMPILER -G "Xcode" ../
 open Tiny3D.xcodeproj
 cd ..
 # cmake --build build_ios --config Release
