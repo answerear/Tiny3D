d4c9688923a781f02235b0a331370a6d097b248d
aaronwang <aaronwang@tencent.com>
2022-04-15 20:07:59 +0800

    added library for math and transformation demo.

---
+++ source/Math/Include/T3DMathPrerequisites.h
@@ -0,0 +1,54 @@
+﻿/*******************************************************************************
+ * This file is part of Tiny3D (Tiny 3D Graphic Rendering Engine)
+ * Copyright (C) 2015-2017  Answer Wong
+ * For latest info, see https://github.com/asnwerear/Tiny3D
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+#ifndef __T3D_MATH_PREREQUISITES_H__
+#define __T3D_MATH_PREREQUISITES_H__
+
+
+#include "T3DMacro.h"
+#include <stdarg.h>
+
+
+#if defined T3DMATH_EXPORT
+    #define T3D_MATH_API        T3D_EXPORT_API
+#else
+    #define T3D_MATH_API        T3D_IMPORT_API
+#endif
+
+
+#include "T3DPlatform.h"
+
+
+namespace Tiny3D
+{
+    class fix32;
+    class fix64;
+
+    template <typename T> class TDegree;
+    template <typename T> class TRadian;
+}
+
+typedef float               float32_t;
+typedef double              float64_t;
+
+typedef Tiny3D::fix32       fix32_t;
+typedef Tiny3D::fix64       fix64_t;
+
+
+#endif    /*__T3D_MATH_PREREQUISITES_H__*/
