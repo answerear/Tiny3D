3aac0dee0595befb7c6413a41f3f9e1672ae0097
answerear <68071730@qq.com>
2022-04-18 14:48:44 +0800

    【ios】修复编译错误

--- source/Platform/Source/Adapter/iOS/T3DiOSDeviceInfo.mm
+++ source/Platform/Source/Adapter/iOS/T3DiOSDeviceInfo.mm
@@ -1,4 +1,4 @@
-﻿/*******************************************************************************
+/*******************************************************************************
  * This file is part of Tiny3D (Tiny 3D Graphic Rendering Engine)
  * Copyright (C) 2015-2017  Answer Wong
  * For latest info, see https://github.com/asnwerear/Tiny3D
@@ -546,7 +546,7 @@ namespace Tiny3D
         return mCPUCores;
     }
 
-    uint32_t iOSDeviceInfo::getSystemRAM() const
+    uint64_t iOSDeviceInfo::getSystemRAM() const
     {
         return mSystemRAM;
     }
