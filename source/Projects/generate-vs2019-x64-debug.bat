7388a774f34ef5a200b9a5ad20ecbdf79c0d9a88
answerear <68071730@qq.com>
2022-11-08 17:49:53 +0800

    【Windows】通过工具自动生成反射代码，并通过 cmake 加入反射代码到工程中，完成一个自动化流程

--- source/Projects/generate-vs2019-x64-debug.bat
+++ source/Projects/generate-vs2019-x64-debug.bat
@@ -1,5 +1,5 @@
 
-@rem Generate base projects.
+@rem ================= Generate base projects =====================
 @cd ..
 @rmdir /Q /S vs2019-x64
 @rmdir /Q /S lib
@@ -7,15 +7,15 @@
 @mkdir vs2019-x64 && cd vs2019-x64
 @cmake -G "Visual Studio 16 2019" -DTINY3D_BUILD_RTTR_TOOL=ON -DCMAKE_BUILD_TYPE=Debug ../
 
-@rem Build ReflectionPreprocessor tool.
+@rem ============== Build ReflectionPreprocessor tool =============
 @cmake --build ./
 
 @cd ..
 @rmdir /Q /S nmake
 @mkdir nmake && cd nmake
 
-@rem System project
-@cmake -G "NMake Makefiles" -DTINY3D_SYSTEM_RTTR=ON -DCMAKE_BUILD_TYPE=Debug ../
+@rem ==================== System project =======================
+@cmake -G "NMake Makefiles" -DTINY3D_SYSTEM_RTTR=ON -UTINY3D_CORE_RTTR -DCMAKE_BUILD_TYPE=Debug ../
 @if not %ERRORLEVEL% == 0 (
 	@cd ../Projects
 	@goto end
@@ -23,17 +23,28 @@
 @rem Copy System compile commands to the project folder.
 xcopy compile_commands.json .\System 
 
+@rem ==================== Core project =========================
+@cmake -G "NMake Makefiles" -DTINY3D_CORE_RTTR=ON -UTINY3D_SYSTEM_RTTR -DCMAKE_BUILD_TYPE=Debug ../
+@if not %ERRORLEVEL% == 0 (
+	@cd ../Projects
+	@goto end
+)
+@rem Copy Core compile commands to the project folder.
+xcopy compile_commands.json .\Core 
 
-@rem ..\..\tools\cct\cct.exe .\System .\Generated
+
+@rem =============== Generate ReflectionSettings.json =============
 @mkdir ..\vs2019-x64\System\Generated
 ..\bin\Windows\Debug\cct.exe .\System ..\vs2019-x64\System\Generated
+@mkdir ..\vs2019-x64\Core\Generated
+..\bin\Windows\Debug\cct.exe .\Core ..\vs2019-x64\Core\Generated
 
 
 @rem Generate reflection source by ReflectionPreprocessor.
-..\bin\Windows\Debug\ReflectionPreprocessor.exe .\System ..\System
+@rem ..\bin\Windows\Debug\ReflectionPreprocessor.exe .\System ..\System
 
 
-@rem Generate all projects.
+@rem ==================== Generate all projects ===================
 @cd ../vs2019-x64
 @cmake -G "Visual Studio 16 2019" -DTINY3D_BUILD_RTTR_TOOL=OFF -DCMAKE_BUILD_TYPE=Debug ../
 
@@ -47,7 +58,7 @@ xcopy compile_commands.json .\System
 )
 
 :end
-@echo Done !
+
 
 
 
