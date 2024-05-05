95fd6e6dfc30cfc9f314f9fe870d5514f6c4536f
answerear <68071730@qq.com>
2024-05-05 23:45:01 +0800

    新增 TinyEditor 工程；新增 Imgui 工程；新增 Imgui 插件工程

---
+++ source/Editor/TinyImGUI/ImGuiDX11/Include/ImGuiDX11Plugin.h
@@ -0,0 +1,68 @@
+﻿/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2024 Answer Wong
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
+
+#pragma once
+
+
+#include "ImGuiDX11Prerequisites.h"
+
+
+namespace Tiny3D
+{
+    class ImGuiDX11Plugin : public Plugin
+    {
+    public:
+        /**
+         * @brief 获取插件名称
+         */
+        const String &getName() const override;
+
+        /**
+         * @brief 安装插件
+         */
+        TResult install() override;
+
+        /**
+         * @brief 启动插件
+         */
+        TResult startup() override;
+
+        /**
+         * @brief 关闭插件
+         */
+        TResult shutdown() override;
+
+        /**
+         * @brief 卸载插件
+         */
+        TResult uninstall() override;
+
+    protected:
+        String mName = "ImGuiDX11";          /**< 插件名称 */
+    };
+
+}
+
+
