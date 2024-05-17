ad19e3fc7be236194d70e837c622500528cc8cb5
answerear <68071730@qq.com>
2024-05-17 20:13:55 +0800

    【Launcher & Editor】提炼下 cmake 文件，统一多语言切换入口

--- source/Editor/TinyEditor/EditorApp.cpp
+++ source/Editor/TinyEditor/EditorApp.cpp
@@ -95,7 +95,8 @@ namespace Tiny3D
 
             // 加载语言文件
             LanguageManagerPtr langMgr = LanguageManager::create();
-            String path = Dir::getAppPath() + Dir::getNativeSeparator() + "Editor" + Dir::getNativeSeparator() + "Language" + Dir::getNativeSeparator() + "lang-zh-hans.txt";
+            String path = Dir::getAppPath() + Dir::getNativeSeparator() + "Editor" + Dir::getNativeSeparator() + "Language" + Dir::getNativeSeparator() + "lang-en-us.txt";
+            // String path = Dir::getAppPath() + Dir::getNativeSeparator() + "Editor" + Dir::getNativeSeparator() + "Language" + Dir::getNativeSeparator() + "lang-zh-hans.txt";
             ret = langMgr->init(path);
             if (T3D_FAILED(ret))
             {
@@ -137,6 +138,8 @@ namespace Tiny3D
             }
             
             ImWidget::GC();
+
+            langMgr = nullptr;
 
             // 删除清理 imgui 环境，此后无法再使用 imgui
             destroyImGuiEnv(engine);
@@ -193,20 +196,20 @@ namespace Tiny3D
 #elif defined (T3D_OS_LINUX)
 #endif
 
-            IM_DELETE(io.Fonts);
-            io.Fonts = IM_NEW(ImFontAtlas);
-            ImFontConfig config;
-            float dpi = 0.0f;
-            if (!SDL_GetDisplayDPI(0, &dpi, nullptr, nullptr))
-                config.RasterizerDensity = dpi / 96.0f;
-            const ImWchar* glyph_ranges = io.Fonts->GetGlyphRangesChineseFull();
-            if (config.RasterizerDensity > 1.0f)
-            {
-                io.Fonts->TexDesiredWidth = (config.RasterizerDensity > 2.0f) ? 4096 * 3 : 4096;
-            }
-            std::string path = Dir::getAppPath() + Dir::getNativeSeparator() + "Editor" + Dir::getNativeSeparator() + "fonts" + Dir::getNativeSeparator() + "STZHONGS.ttf";
-            io.Fonts->AddFontFromFileTTF(path.c_str(), 20.0f, &config, glyph_ranges);
-            io.Fonts->Build();
+            // IM_DELETE(io.Fonts);
+            // io.Fonts = IM_NEW(ImFontAtlas);
+            // ImFontConfig config;
+            // float dpi = 0.0f, xdpi, ydpi;
+            // if (!SDL_GetDisplayDPI(0, &dpi, &xdpi, &ydpi))
+            //     config.RasterizerDensity = 1.5f;//xdpi / 96.0f;
+            // const ImWchar* glyph_ranges = io.Fonts->GetGlyphRangesChineseFull();
+            // if (config.RasterizerDensity > 1.0f)
+            // {
+            //     io.Fonts->TexDesiredWidth = (config.RasterizerDensity > 2.0f) ? 4096 * 3 : 4096;
+            // }
+            // std::string path = Dir::getAppPath() + Dir::getNativeSeparator() + "Editor" + Dir::getNativeSeparator() + "fonts" + Dir::getNativeSeparator() + "arial unicode ms.ttf";
+            // io.Fonts->AddFontFromFileTTF(path.c_str(), 20.0f, &config, glyph_ranges);
+            // io.Fonts->Build();
         } while (false);
 
         return ret;
