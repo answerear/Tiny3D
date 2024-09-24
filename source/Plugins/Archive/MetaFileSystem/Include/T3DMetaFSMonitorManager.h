ab61f40e86a213c3c833118d120ea9d166bc575c
answerear <68071730@qq.com>
2024-09-24 13:59:32 +0800

    【MetaFSArchive】调整内部实现，可以通过 uuid 跨 archive 对象访问文件

---
+++ source/Plugins/Archive/MetaFileSystem/Include/T3DMetaFSMonitorManager.h
@@ -0,0 +1,55 @@
+﻿/*******************************************************************************
+ * This file is part of Tiny3D (Tiny 3D Graphic Rendering Engine)
+ * Copyright (C) 2015-2020  Answer Wong
+ * For latest info, see https://github.com/answerear/Tiny3D
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+
+#pragma once
+
+
+#include "T3DMetaFSArchivePrerequisites.h"
+
+
+namespace Tiny3D
+{
+    class MetaFSMonitorManager : public Singleton<MetaFSMonitorManager>
+    {
+    public:
+        ~MetaFSMonitorManager() override;
+
+        TResult addMonitor(const String &path);
+
+        TResult removeMonitor(const String &path);
+
+        TResult removeAllMonitors();
+
+        const String &getPath(const UUID &uuid) const;
+
+        MetaFSMonitor *getMonitor(const UUID &uuid) const;
+
+        bool getPathAndMonitor(const UUID &uuid, String &path, MetaFSMonitor *&monitor);
+        
+    protected:
+        using PathMonitors = TUnorderedMap<String, MetaFSMonitorPtr>;
+
+        /// 根路径集合，用于自动检测是否生成 meta 文件
+        PathMonitors mPathMonitors {};
+    };
+
+    #define T3D_MFS_MONITOR_MGR (MetaFSMonitorManager::getInstance())
+}
+
---
+++ source/Plugins/Archive/MetaFileSystem/Source/T3DMetaFSMonitorManager.cpp
@@ -0,0 +1,166 @@
+﻿/*******************************************************************************
+ * This file is part of Tiny3D (Tiny 3D Graphic Rendering Engine)
+ * Copyright (C) 2015-2020  Answer Wong
+ * For latest info, see https://github.com/answerear/Tiny3D
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+
+#include "T3DMetaFSArchivePrerequisites.h"
+#include "T3DMetaFSMonitorManager.h"
+#include "T3DMetaFSMonitor.h"
+
+
+namespace Tiny3D
+{
+    //--------------------------------------------------------------------------
+
+    MetaFSMonitorManager::~MetaFSMonitorManager()
+    {
+        removeAllMonitors();
+    }
+
+    //--------------------------------------------------------------------------
+
+    TResult MetaFSMonitorManager::addMonitor(const String &path)
+    {
+        TResult ret = T3D_OK;
+
+        do
+        {
+            MetaFSMonitorPtr monitor;
+                
+            // 找出是否已经有启动监控的，子文件夹都在监控中，不重复监控
+            for (const auto &item : mPathMonitors)
+            {
+                if (item.first == path)
+                {
+                    // 完全相同路径
+                    monitor = item.second;
+                    break;
+                }
+
+                // String::size_type pos = name.find_first_not_of(item.first);
+                bool match = StringUtil::match(path, item.first, false);
+                if (match)
+                {
+                    // 包含了部分路径，name 是其子路径
+                    monitor = item.second;
+                    break;
+                }
+            }
+                    
+            if (monitor == nullptr)
+            {
+                // 创建监控对象
+                monitor = MetaFSMonitor::create(path);
+                ret = monitor->init();
+                if (T3D_FAILED(ret))
+                {
+                    MFS_LOG_ERROR("Failed to initialize monitor %s.", path.c_str());
+                    break;
+                }
+
+                mPathMonitors.emplace(path, monitor);
+            }
+        } while (false);
+        
+        return ret;
+    }
+
+    //--------------------------------------------------------------------------
+
+    TResult MetaFSMonitorManager::removeMonitor(const String &path)
+    {
+        TResult ret = T3D_OK;
+
+        mPathMonitors.erase(path);
+        
+        return ret;
+    }
+
+    //--------------------------------------------------------------------------
+
+    TResult MetaFSMonitorManager::removeAllMonitors()
+    {
+        for (const auto &item : mPathMonitors)
+        {
+            item.second->stop();
+        }
+        mPathMonitors.clear();
+        return T3D_OK;
+    }
+
+    //--------------------------------------------------------------------------
+
+    const String &MetaFSMonitorManager::getPath(const UUID &uuid) const
+    {
+        for (const auto &item : mPathMonitors)
+        {
+            const String &path = item.second->getPath(uuid);
+            if (!path.empty())
+            {
+                return path;
+            }
+        }
+
+        static String EMPTY;
+        return EMPTY;
+    }
+
+    //--------------------------------------------------------------------------
+
+    MetaFSMonitor *MetaFSMonitorManager::getMonitor(const UUID &uuid) const
+    {
+        MetaFSMonitor *monitor = nullptr;
+        
+        for (const auto &item : mPathMonitors)
+        {
+            const String &path = item.second->getPath(uuid);
+            if (!path.empty())
+            {
+                monitor = item.second;
+                break;
+            }
+        }
+        
+        return monitor;
+    }
+
+    //--------------------------------------------------------------------------
+
+    bool MetaFSMonitorManager::getPathAndMonitor(const UUID &uuid, String &path, MetaFSMonitor *&monitor)
+    {
+        bool ret = false;
+        monitor = nullptr;
+
+        for (const auto &item : mPathMonitors)
+        {
+            const String &p = item.second->getPath(uuid);
+            if (!p.empty())
+            {
+                monitor = item.second;
+                path = p;
+                ret = true;
+                break;
+            }
+        }
+        
+        return ret;
+    }
+
+    //--------------------------------------------------------------------------
+}
+
