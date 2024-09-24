a7ccb2fa7d9e27e9b220a24e801cd1821e2e8fef
answerear <68071730@qq.com>
2024-09-24 08:52:00 +0800

    【TinyEditor】加入测试场景

---
+++ source/Editor/TinyEditor/TestScene.h
@@ -0,0 +1,59 @@
+﻿/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2024 Answer Wong
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
+#pragma once
+
+
+#include "EditorPrerequisites.h"
+
+
+namespace Tiny3D
+{
+    NS_BEGIN(Editor)
+
+    class TestScene : public Singleton<TestScene>
+    {
+    public:
+        TestScene() = default;
+        
+        ~TestScene() override;
+
+        void build(EditorScene *editorScene);
+
+    protected:  // for test
+        void buildCamera(Transform3D *parent);
+        void buildCube(Transform3D *parent);
+        Texture2DPtr buildTexture();
+        MaterialPtr buildMaterial();
+        MeshPtr buildMesh();
+        void buildAabb(Mesh *mesh, SubMesh *submesh, AabbBound *bound);
+        
+    protected:
+        EditorScene *mEditorScene {nullptr};
+    };
+
+    #define TEST_SCENE (TestScene::getInstance())
+
+    NS_END
+}
