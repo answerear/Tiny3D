65dd5c261e47b6f14e505b2533702c534388f9b5
answerear <68071730@qq.com>
2024-11-15 12:42:07 +0800

    【TinyImGui】新增菜单事件管理器，用于统一处理菜单事件，提升菜单响应性能

---
+++ source/Editor/TinyImGui/Include/ImMenuEventHandler.h
@@ -0,0 +1,108 @@
+﻿/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2024 Answer Wong
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
+
+#pragma once
+
+
+#include "ImPrerequisites.h"
+#include "ImMenu.h"
+
+
+namespace Tiny3D
+{
+    #define ON_MENU_ITEM(item_id, func) registerMenuEvent(item_id, func)
+    #define ON_MENU_ITEM_MEMBER(item_id, func) ON_MENU_ITEM(item_id, std::bind(&func, this, std::placeholders::_1, std::placeholders::_2))
+
+    #define ON_MENU_ITEM_QUERY(item_id, func) setMenuItemQueryEnabledHandle(item_id, func)
+    #define ON_MENU_ITEM_QUERY_MEMBER(item_id, func) ON_MENU_ITEM_QUERY(item_id, std::bind(&func, this, std::placeholders::_1, std::placeholders::_2))
+
+    #define IM_MENU_ITEM_DEFAULT_QUERY_ENABLED()    std::bind(&ImMenuEventManager::onQueryEnabled, ImMenuEventManager::getInstancePtr(), std::placeholders::_1)
+    #define IM_MENU_ITEM_DEFAULT_QUERY_CHECKED()    std::bind(&ImMenuEventManager::onQueryChecked, ImMenuEventManager::getInstancePtr(), std::placeholders::_1)
+
+    #define IM_MENU_ITEM_DATA_DEFAULT(type, menuID, title, shortcut, icon_name) \
+        IM_MENU_ITEM_DATA(type, menuID, title, shortcut, icon_name, IM_MENU_ITEM_DEFAULT_QUERY_ENABLED(), IM_MENU_ITEM_DEFAULT_QUERY_CHECKED(), nullptr)
+    
+    using ImMenuEventHandle = TFunction<bool(uint32_t, ImWidget*)>;
+    using ImMenuQueryHandle = TFunction<bool(uint32_t, ImWidget*)>;
+    
+    class TINYIMGUI_API ImMenuEventHandler
+    {
+    public:
+        virtual ~ImMenuEventHandler();
+
+    protected:
+        TResult registerMenuEvent(uint32_t menuID, const ImMenuEventHandle &handle);
+
+        TResult unregisterMenuEvent(uint32_t menuID);
+
+        TResult setMenuItemQueryEnabledHandle(uint32_t menuID, const ImMenuQueryHandle &handle);
+
+        TResult setMenuItemQueryCheckedHandle(uint32_t menuID, const ImMenuQueryHandle &handle);
+    };
+
+    class TINYIMGUI_API ImMenuEventManager
+        : public EventHandler
+        , public Singleton<ImMenuEventManager>
+    {
+        friend class ImMenuEventHandler;
+        
+    public:
+        ImMenuEventManager();
+
+        ~ImMenuEventManager() override;
+
+        TResult setMenuItemQueryEnabledHandle(uint32_t menuID, const ImMenuQueryHandle &handle);
+
+        TResult setMenuItemQueryCheckedHandle(uint32_t menuID, const ImMenuQueryHandle &handle);
+
+        bool onQueryEnabled(ImWidget *menuItem);
+
+        bool onQueryChecked(ImWidget *menuItem);
+
+    protected:
+        TResult registerMenuEvent(uint32_t menuID, ImMenuEventHandler *handler, const ImMenuEventHandle &handle);
+
+        TResult unregisterMenuEvent(uint32_t menuID, ImMenuEventHandler *handler);
+
+        TResult unregisterMenuEventHandler(ImMenuEventHandler *handler);
+
+        bool onHandleMenuItem(EventParam *param, TINSTANCE sender);
+
+    protected:
+        using ImMenuItemHandles = TUnorderedMultimap<ImMenuEventHandler*, ImMenuEventHandle>;        
+        using ImMenuItemHandlers = TUnorderedMultimap<uint32_t, ImMenuItemHandles>;
+
+        using ImMenuItemQueryHandles = TUnorderedMap<uint32_t, ImMenuQueryHandle>;
+        
+        /// 点击菜单项的回调集合
+        ImMenuItemHandlers mMenuItemHandlers {};
+
+        /// 查询菜单项是否可点击回调集合
+        ImMenuItemQueryHandles mMenuItemQueryEnabledHandles {};
+
+        /// 查询菜单项是否check状态回调集合
+        ImMenuItemQueryHandles mMenuItemQueryCheckedHandles {};
+    };
+}
---
+++ source/Editor/TinyImGui/Source/ImMenuEventHandler.cpp
@@ -0,0 +1,217 @@
+﻿/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2024 Answer Wong
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
+
+#include "ImMenuEventHandler.h"
+#include "ImEventDefine.h"
+#include "ImMenu.h"
+
+
+namespace Tiny3D
+{
+    //--------------------------------------------------------------------------
+
+    ImMenuEventHandler::~ImMenuEventHandler()
+    {
+        ImMenuEventManager::getInstance().unregisterMenuEventHandler(this);
+    }
+    
+    //--------------------------------------------------------------------------
+
+    TResult ImMenuEventHandler::registerMenuEvent(uint32_t menuID, const ImMenuEventHandle &handle)
+    {
+        return ImMenuEventManager::getInstance().registerMenuEvent(menuID, this, handle);
+    }
+    
+    //--------------------------------------------------------------------------
+
+    TResult ImMenuEventHandler::unregisterMenuEvent(uint32_t menuID)
+    {
+        return ImMenuEventManager::getInstance().unregisterMenuEvent(menuID, this);
+    }
+
+    //--------------------------------------------------------------------------
+
+    TResult ImMenuEventHandler::setMenuItemQueryEnabledHandle(uint32_t menuID, const ImMenuQueryHandle &handle)
+    {
+        return ImMenuEventManager::getInstance().setMenuItemQueryEnabledHandle(menuID, handle);
+    }
+    
+    //--------------------------------------------------------------------------
+
+    TResult ImMenuEventHandler::setMenuItemQueryCheckedHandle(uint32_t menuID, const ImMenuQueryHandle &handle)
+    {
+        return ImMenuEventManager::getInstance().setMenuItemQueryCheckedHandle(menuID, handle);
+    }
+
+    //--------------------------------------------------------------------------
+
+    ImMenuEventManager::ImMenuEventManager()
+    {
+        ON_MEMBER(kEvtMenuItemClicked, ImMenuEventManager::onHandleMenuItem);
+    }
+
+    //--------------------------------------------------------------------------
+
+    ImMenuEventManager::~ImMenuEventManager()
+    {
+        unregisterAllEvent();
+    }
+
+    //--------------------------------------------------------------------------
+
+    TResult ImMenuEventManager::setMenuItemQueryEnabledHandle(uint32_t menuID, const ImMenuQueryHandle &handle)
+    {
+        mMenuItemQueryEnabledHandles.emplace(menuID, handle);
+        return T3D_OK;
+    }
+
+    //--------------------------------------------------------------------------
+
+    TResult ImMenuEventManager::setMenuItemQueryCheckedHandle(uint32_t menuID, const ImMenuQueryHandle &handle)
+    {
+        mMenuItemQueryCheckedHandles.emplace(menuID, handle);
+        return T3D_OK;
+    }
+
+    //--------------------------------------------------------------------------
+
+    bool ImMenuEventManager::onQueryEnabled(ImWidget *menuItem)
+    {
+        bool enabled = false;
+        
+        const auto itr = mMenuItemQueryEnabledHandles.find(menuItem->getID());
+        if (itr != mMenuItemQueryEnabledHandles.end())
+        {
+            enabled = itr->second(menuItem->getID(), menuItem);
+        }
+        
+        return enabled;
+    }
+
+    //--------------------------------------------------------------------------
+
+    bool ImMenuEventManager::onQueryChecked(ImWidget *menuItem)
+    {
+        bool checked = false;
+
+        const auto itr = mMenuItemQueryCheckedHandles.find(menuItem->getID());
+        if (itr != mMenuItemQueryCheckedHandles.end())
+        {
+            checked = itr->second(menuItem->getID(), menuItem);
+        }
+        
+        return checked;
+    }
+    
+    //--------------------------------------------------------------------------
+
+    TResult ImMenuEventManager::registerMenuEvent(uint32_t menuID, ImMenuEventHandler *handler, const ImMenuEventHandle &handle)
+    {
+        TResult ret = T3D_OK;
+
+        do
+        {
+            const auto itr = mMenuItemHandlers.find(menuID);
+            if (itr == mMenuItemHandlers.end())
+            {
+                // 从来没加过菜单项，则新生成一个菜单项处理对象和函数列表
+                auto it = mMenuItemHandlers.emplace(menuID, ImMenuItemHandles());
+                it->second.emplace(handler, handle);
+            }
+            else
+            {
+                itr->second.emplace(handler, handle);
+            }
+        } while (false);
+
+        return ret;
+    }
+    
+    //--------------------------------------------------------------------------
+
+    TResult ImMenuEventManager::unregisterMenuEvent(uint32_t menuID, ImMenuEventHandler *handler)
+    {
+        TResult ret = T3D_OK;
+
+        do
+        {
+            const auto itr = mMenuItemHandlers.find(menuID);
+            if (itr != mMenuItemHandlers.end())
+            {
+                itr->second.erase(handler);
+            }
+            else
+            {
+                ret = T3D_ERR_NOT_FOUND;
+            }
+        } while (false);
+        
+        return ret;
+    }
+
+    //--------------------------------------------------------------------------
+
+    TResult ImMenuEventManager::unregisterMenuEventHandler(ImMenuEventHandler *handler)
+    {
+        TResult ret = T3D_OK;
+
+        do
+        {
+            for (auto itr = mMenuItemHandlers.begin(); itr != mMenuItemHandlers.end(); ++itr)
+            {
+                itr->second.erase(handler);
+            }
+        } while (false);
+
+        return ret;
+    }
+
+    //--------------------------------------------------------------------------
+
+    bool ImMenuEventManager::onHandleMenuItem(EventParam *param, TINSTANCE sender)
+    {
+        bool ret = true;
+        EventParamMenuItem *p = static_cast<EventParamMenuItem *>(param);
+
+        uint32_t menuID = p->arg1->getID();
+        const auto itr = mMenuItemHandlers.find(menuID);
+        if (itr != mMenuItemHandlers.end())
+        {
+            if (!itr->second.empty())
+            {
+                for (auto it = itr->second.begin(); it != itr->second.end(); ++it)
+                {
+                    EventHandler *obj = nullptr;
+                    EventManager::getInstance().getEventHandler(sender, obj);
+                    ret = ret && it->second(menuID, (ImWidget *)obj);
+                }
+            }
+        }
+        
+        return ret;
+    }
+
+    //--------------------------------------------------------------------------
+}
