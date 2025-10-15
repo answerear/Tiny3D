955954ea3a80bf9f35169faf9e4bc40081f14d9a
answerear <68071730@qq.com>
2025-10-15 14:50:48 +0800

    【ReousrceApp】新增 sample，用于展示和验证资源管理功能

---
+++ source/Samples/ResourceApp/ResourceApp.cpp
@@ -0,0 +1,181 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2024 Answer Wong
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
+#include "ResourceApp.h"
+
+
+#define UVN_CAMERA
+
+#define USE_GPU_SKIN
+
+using namespace Tiny3D;
+
+
+ResourceApp theApp;
+
+
+ResourceApp::ResourceApp()
+{
+}
+
+ResourceApp::~ResourceApp()
+{
+}
+
+TResult ResourceApp::go(int32_t argc, char *argv[])
+{
+    TResult ret;
+
+    Agent *theEngine = T3D_NEW Agent();
+    Settings settings;
+    settings.pluginSettings.pluginPath = ".";
+    settings.pluginSettings.plugins.push_back("FileSystemArchive");
+    settings.pluginSettings.plugins.push_back("MetaFSArchive");
+    settings.pluginSettings.plugins.push_back("D3D11Renderer");
+    settings.pluginSettings.plugins.push_back("FreeImageCodec");
+
+#if defined (T3D_OS_WINDOWS)
+    settings.renderSettings.renderer = RHIRenderer::DIRECT3D11;
+#else
+#endif
+    
+    ret = theEngine->init(argc, argv, true, true, settings);
+    if (ret == T3D_OK)
+        theEngine->run();
+
+    T3D_SAFE_DELETE(theEngine);
+
+    return ret;
+}
+
+TResult ResourceApp::applicationDidFinishLaunching(int32_t argc, char *argv[])
+{
+    T3D_AGENT.loadPlugin("MetaFSArchive");
+    
+    // create scene
+    ScenePtr scene = T3D_SCENE_MGR.createScene("TestScene");
+    scene->init();
+    T3D_SCENE_MGR.setCurrentScene(scene);
+
+    // add ambient light to the root of scene
+    AmbientLightPtr ambient = scene->getRootGameObject()->addComponent<AmbientLight>();
+    ambient->setColor(ColorRGB::WHITE);
+    ambient->setIntensity(0.5f);
+    
+    // root game object
+    GameObjectPtr go = GameObject::create("TestScene");
+    // scene->addRootGameObject(go);
+    Transform3DPtr root = go->addComponent<Transform3D>();
+    scene->getRootTransform()->addChild(root);
+
+    // directional light
+    go = GameObject::create("DirectionLight");
+    Transform3DPtr node = go->addComponent<Transform3D>();
+    // Quaternion q(Radian(Math::PI * 0.5f), Vector3::UNIT_X);
+    Vector3 eye(-2.0f, 2.0f, -4.0f);
+    Matrix3 matR;
+    matR.lookAt_LH(eye, Vector3::ZERO, Vector3::UP);
+    Quaternion q(matR);
+    node->setOrientation(q);
+    node->setPosition(eye);
+    root->addChild(node);
+    DirectionalLightPtr light = go->addComponent<DirectionalLight>();
+    light->setColor(ColorRGB::WHITE);
+    light->setDiffuseIntensity(1.0f);
+    light->setSpecularIntensity(1.0f);
+
+    // camera
+    buildCamera(root);
+
+    // mesh
+    loadMesh(root);
+    
+    return T3D_OK;
+}
+
+void ResourceApp::applicationWillTerminate() 
+{
+    mCubeMesh = nullptr;
+    mPlaneMesh = nullptr;
+    mCubeMaterial = nullptr;
+    mPlaneMaterial = nullptr;
+}
+
+void ResourceApp::buildCamera(Transform3D *parent)
+{
+    // render window for render target in camera
+    RenderWindowPtr rw = T3D_AGENT.getDefaultRenderWindow();
+    RenderTargetPtr rt = RenderTarget::create(rw);
+
+    // transform node for camera
+    GameObjectPtr go = GameObject::create("Camera");
+    Transform3DPtr xform = go->addComponent<Transform3D>();
+    parent->addChild(xform);
+    
+    // camera component
+    CameraPtr camera = go->addComponent<Camera>();
+    camera->setOrder(0);
+    Viewport vp {0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f};
+    camera->setViewport(vp);
+    camera->setClearColor(ColorRGB::BLACK);
+    camera->setRenderTarget(rt);
+    
+    // camera for perspective
+    camera->setProjectionType(Camera::Projection::kPerspective);
+    camera->setFovY(Radian(Math::PI / 3.0f));
+    // camera->setFovY(Radian(Math::PI * 0.5f));
+    Real as = Real(rw->getDescriptor().Width) / Real(rw->getDescriptor().Height);
+    camera->setAspectRatio(as);
+    camera->setNearPlaneDistance(0.1f);
+    camera->setFarPlaneDistance(1000.0f);
+    
+    // construct camera position & orientation & scaling
+
+    Vector3 eye(8.0f, 20.0f, -20.0f);
+
+#if defined (UVN_CAMERA)
+    Vector3 obj(0.0f, 0.0f, 0.0f);
+    camera->lookAt(eye, obj, Vector3::UP);
+#else
+    xform->setPosition(eye);
+    Radian xAngle(Degree(25.0f).valueRadians());
+    Radian yAngle(-Math::PI * 0.25f);
+    // Radian yAngle(0.0f);
+    Radian zAngle(0.0f);
+    xform->fromEulerAnglesYXZ(yAngle, xAngle, zAngle);
+#endif
+
+    // construct frustum bound
+    auto frustum = go->addComponent<FrustumBound>();
+    T3D_ASSERT(frustum != nullptr);
+}
+
+void ResourceApp::loadMesh(Transform3D *parent)
+{
+    
+}
+
+
+
+
---
+++ source/Samples/ResourceApp/ResourceApp.h
@@ -0,0 +1,58 @@
+/*******************************************************************************
+* MIT License
+ *
+ * Copyright (c) 2024 Answer Wong
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
+#ifndef __RESOURCE_APP_H__
+#define __RESOURCE_APP_H__
+
+
+#include "../Common/SampleApp.h"
+
+
+class ResourceApp : public SampleWindowApp
+{
+public:
+    ResourceApp();
+    ~ResourceApp() override;
+
+    TResult go(int32_t argc, char *argv[]) override;
+    
+protected:  /// from Tiny3D::Application
+    TResult applicationDidFinishLaunching(int32_t argc, char *argv[]) override;
+
+    void applicationWillTerminate() override;
+
+protected:
+    void buildCamera(Tiny3D::Transform3D *parent);
+
+    void loadMesh(Tiny3D::Transform3D *parent);
+    
+    Tiny3D::SkinnedMeshPtr mCubeMesh {nullptr};
+    Tiny3D::MeshPtr mPlaneMesh {nullptr};
+    
+    Tiny3D::MaterialPtr mCubeMaterial {nullptr};
+    Tiny3D::MaterialPtr mPlaneMaterial {nullptr};
+};
+
+
+#endif  /*__RESOURCE_APP_H__*/
