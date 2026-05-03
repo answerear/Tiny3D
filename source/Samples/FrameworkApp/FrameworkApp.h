f063241294e55cd12ce1ef37cf8bad203b688a05
answerear <68071730@qq.com>
2026-05-03 09:37:44 +0800

    【Source】把非 UTF-8 BOM 格式统一改成 UTF-8 BOM 格式

--- source/Samples/FrameworkApp/FrameworkApp.h
+++ source/Samples/FrameworkApp/FrameworkApp.h
@@ -1,4 +1,4 @@
-/*******************************************************************************
+﻿/*******************************************************************************
  * This file is part of Tiny3D (Tiny 3D Graphic Rendering Engine)
  * Copyright (C) 2015-2020  Answer Wong
  * For latest info, see https://github.com/answerear/Tiny3D
