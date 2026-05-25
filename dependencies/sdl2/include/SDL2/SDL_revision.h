f0c7d162f226e255da0be9401ea871bdc08ebbd4
answerear <68071730@qq.com>
2026-05-25 17:39:34 +0800

    【SDL2】更新 SDL2 库，版本为  2.32.11 版本

--- dependencies/sdl2/include/SDL2/SDL_revision.h
+++ dependencies/sdl2/include/SDL2/SDL_revision.h
@@ -1,2 +1,8 @@
-#define SDL_REVISION "hg-11645:2088cd828335"
-#define SDL_REVISION_NUMBER 11645
+/* #undef SDL_VENDOR_INFO */
+#define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-release-2.32.10-82-gbb6544657 (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-release-2.32.10-82-gbb6544657"
+#endif
