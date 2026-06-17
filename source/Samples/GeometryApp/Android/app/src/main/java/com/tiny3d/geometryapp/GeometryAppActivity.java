4c2e9d5c1537235d1368429187ec890d2775a61b
answerear <68071730@qq.com>
2026-06-17 13:00:42 +0800

    【Samples - GeometryApp】支持 android 平台，正常渲染

--- source/Samples/GeometryApp/Android/app/src/main/java/com/tiny3d/geometryapp/GeometryAppActivity.java
+++ source/Samples/GeometryApp/Android/app/src/main/java/com/tiny3d/geometryapp/GeometryAppActivity.java
@@ -1,26 +1,28 @@
 package com.tiny3d.geometryapp;
 
 import com.tiny3d.lib.Tiny3DActivity;
-
 import org.libsdl.app.SDLActivity;
-
-/**
- * Created by aaronwang on 2018/1/6.
- */
 
 public class GeometryAppActivity extends Tiny3DActivity {
     static {
         System.loadLibrary("T3DPlatform");
         System.loadLibrary("T3DCore");
-        System.loadLibrary("T3DLog");
-        System.loadLibrary("T3DMath");
         System.loadLibrary("GeometryApp");
     }
 
+    @Override
+    protected String[] getLibraries() {
+        return new String[] {
+            "SDL2",
+        };
+    }
+
+    @Override
     protected String getMainSharedObject() {
         return "libGeometryApp.so";
     }
 
+    @Override
     protected String getMainFunction() {
         return "main";
     }
