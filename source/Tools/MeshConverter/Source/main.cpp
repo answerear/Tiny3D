405a4d38ca891536be771da476d50118d2e911d2
answerear <68071730@qq.com>
2026-07-29 17:04:18 +0800

    【Tools - MeshConverter】修改注释

--- source/Tools/MeshConverter/Source/main.cpp
+++ source/Tools/MeshConverter/Source/main.cpp
@@ -21,8 +21,8 @@
 #include "T3DMeshConverterApp.h"
 
 // For debug
-// Text : -o tmesh -b sphere -t -a -r $(SolutionDir)..\..\assets\editor\builtin -d materials\Tiny3DStandard.tmat $(SolutionDir)..\..\assets\samples\raw\tortoise.fbx $(SolutionDir)..\..\assets\samples\meshes
-// Text : -o Tiny3D -b sphere -t -a -r $(SolutionDir)..\..\assets\editor\builtin -d materials\Tiny3DStandard.tmat $(SolutionDir)..\..\assets\samples\raw\tortoise.fbx $(SolutionDir)..\..\assets\samples\meshes
+// Test : -o tmesh -b sphere -t -a -r $(SolutionDir)..\..\assets\editor\builtin -d materials\Tiny3DStandard.tmat $(SolutionDir)..\..\assets\samples\raw\tortoise.fbx $(SolutionDir)..\..\assets\samples\meshes
+// Test : -o Tiny3D -b sphere -t -a -r $(SolutionDir)..\..\assets\editor\builtin -d materials\Tiny3DStandard.tmat $(SolutionDir)..\..\assets\samples\raw\tortoise.fbx $(SolutionDir)..\..\assets\samples\meshes
 
 
 int main(int argc, char *argv[])
